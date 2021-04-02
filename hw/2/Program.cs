496e7493cb7c3fb3a9be38dd993276f324d36374
Longyde <77998284+Longyde@users.noreply.github.com>
2021-04-02 11:07:16 -0700

    Wepage update1

---
+++ hw/2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace probl2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            Console.WriteLine("Eneter two numbers to divide(we recommend NOT by zero): ");
+            int firstArg = Convert.ToInt32(Console.ReadLine());
+            int secondArg = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Swappa.accept(ref firstArg, ref secondArg);
+            }
+            catch (DivideByZeroException)
+            {
+
+                Console.WriteLine("Told you not to divide by Zero.");
+                goto start;
+            }
+            
+        }
+    }
+    class Swappa
+    {
+        public static int accept(ref int a, ref int b)
+        {
+            
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / b;
+
+        }
+    }
+}
---
+++ hw/45/cars.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace defaultp
+{
+    class cars
+    {
+        private string model;
+        private string make;
+        private int year;
+        public string Make
+        {
+            get 
+            { 
+                return make; 
+            }
+            set 
+            { 
+                make = "Tesla"; 
+            }
+        }
+        public string Model
+        {
+            get 
+            { 
+                return model; 
+            }
+            set 
+            { 
+                model = "Model 3"; 
+            }
+        }
+        int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+        public void Car1(string ma, string mo, int y)
+        {
+            
+            this.Make = Make;
+            this.Model = Model;
+
+            Console.WriteLine($"Well, your car is a {y} {Make} {Model}. Good Make and Model!!");
+        }
+    }
+}
