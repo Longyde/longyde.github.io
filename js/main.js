b763cc4c750852acd6a4a9d0b23c71d565d49a95
Devonte M Longmire <77998284+Longyde@users.noreply.github.com>
2025-11-02 15:51:26 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
 
 consoleText([
   '',
+  'Welcome!'.
+  'My name is Devonte Longmire',
   'Building since 1999 — now with purpose.',
   'Raspberry Pi tinkerer. AI collaborator.',
   'Software engineer and creative problem solver.',
